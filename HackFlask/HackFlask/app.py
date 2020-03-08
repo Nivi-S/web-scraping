5d00c79dc786e1b3b2dd021fd10def6a10fdbf9d
youngeul <54693098+youngeul@users.noreply.github.com>
2020-03-08 03:19:13 -0700

    Add files via upload

--- HackFlask/HackFlask/app.py
+++ HackFlask/HackFlask/app.py
@@ -1,32 +1,51 @@
 from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def hello():
-    return render_template("index.html")
+    return render_template("index.html", data="testtttt")
 
+@app.route("/test/", methods=['GET', 'POST'])
+def test():
+    data = "testtttt"
+    return data
 
-@app.route("/UCLA/")
-def getUCLAnews():
-    ()
+# @app.route("/UCLA/")
+# def getUCLAnews():
+#     # ()
+#
+#
+# @app.route("/USC/")
+# def getUSCnews():
+#     return parseHTML('https://sites.usc.edu/coronavirus/')
+#
+#
+# @app.route("/LAC/")
+# def getLACnews():
+#
+#
+# @app.route("/CA/")
+# def getCAnews():
+#
+#
+# @app.route("/CDC/")
+# def getCDCnews():
 
-
-@app.route("/USC/")
-def getUSCnews():
-    return parseHTML('https://sites.usc.edu/coronavirus/')
-    
-
-@app.route("/LAC/")
-def getLACnews():
-
-@app.route("/CA/")
-def getCAnews():
-
-@app.route("/CDC/")
-def getCDCnews():
-
-    
 
 if __name__ == "__main__":
-	app.run(debug = True, host='0.0.0.0', port=8080, passthrough_errors=True)
+    app.run(debug=True, host='0.0.0.0', port=8080, passthrough_errors=True)
+
+# from flask import Flask, render_template, request
+# app = Flask(__name__)
+#
+#
+# @app.route("/")
+# def hello():
+#     return render_template("index.html")
+#
+#
+# if __name__ == "__main__":
+# 	app.run(debug = True, host='0.0.0.0', port=8080, passthrough_errors=True)
+
