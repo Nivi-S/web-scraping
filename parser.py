4acc7a01a10d90f4f4293d7ee79acc837631279f
Niv <toniveditasingh@gmail.com>
2020-03-07 21:13:44 -0800

    updated parser

--- parser.py
+++ parser.py
@@ -3,64 +3,58 @@ from bs4 import BeautifulSoup
 import re
 import requests
 import pressSource as ps
+import urllib3
+#import flask
 
-# driver = webdriver.Chrome()
-# url = "https://www.cdc.gov/coronavirus/2019-ncov/cases-in-us.html"
-# driver.get("https://www.cdc.gov/coronavirus/2019-ncov/cases-in-us.html")
-# content = driver.page_source
 
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 def parseHTML(str_url):
-    content = requests.get(str_url)
+
+    content = requests.get(str_url, verify = False)
     soup = BeautifulSoup(content.text, 'html.parser')
 
-    searched_word = 'Total'
+
+    if bool(re.search('.*cdc.*', str_url)):
+        searched_word = 'Total'
+
+    if bool(re.search('.*usc.*', str_url)):
+        searched_word = 'STATUS'
+
+    if bool(re.search('.*lacounty.*', str_url)):
+        searched_word = 'total'
+
+    if bool(re.search('.*ucla.*', str_url)):
+        searched_word = 'STATUS'
+
     results = soup.find_all(string=re.compile(
         '.*{0}.*'.format(searched_word)), recursive=True)
+    print('results =', results)
+
+    date = soup.find_all(string=re.compile('[ADFJMNOS]\w* [\d]{1,2},* [\d]{4}'))
+    print("Date=", date[0])
 
     for content in results:
         words = content.split()
         for index, word in enumerate(words):
             # If the content contains the search word twice or more this will fire for each occurence
             if word == searched_word:
-                print('Results: "{0}"'.format(content))
+                print("Content= ", content, "\n")#('Results: "{0}"'.format(content))
 
-    print(soup.title.text)
+    #print("\n Title =", soup.title.text)
+    print("Source =", str_url, "\n ================ \n")
+    #CDC = ps.pressSource(ps.level.NATIONAL, str_url)
 
-    CDC = ps.pressSource(ps.level.NATIONAL, str_url)
+
 
 
 if __name__ == "__main__":
     url = 'https://www.cdc.gov/coronavirus/2019-ncov/cases-in-us.html'
     parseHTML(url)
+    parseHTML('https://sites.usc.edu/coronavirus/')
+    parseHTML('http://publichealth.lacounty.gov/media/Coronavirus/')
+    #parseHTML('https://www.uclahealth.org/body.cfm?id=1397&action=detail&ref=3976&fr=true')
+    parseHTML('https://newsroom.ucla.edu/stories/coronavirus-information-for-the-ucla-campus-community')
 
 
 # ============================
-
-# next_word = 'reporting'
-# next_results = soup.find_all(string=re.compile('.*{0}.*'.format(next_word)), recursive=True)
-
-
-# for cont in next_results:
-#     words = cont.split()
-#     for index, word in enumerate(words):
-#         # If the content contains the search word twice or more this will fire for each occurence
-#         if word == next_word:
-#             print ('States: "{0}"'.format(cont))
-
-
-# for tag in cases.find_all("li"):
-# 	print("{0}: {1}".format(tag.name, tag.text))
-
-# =========================================
-
-# for a in table.findAll('td', {'rowspan'}):
-# 	timestamp=a.find('span', class_='nowrap')
-# 	printf(timestamp.extract())
-# 	#occurance=a.find('td', attrs={'rowspan'})
-# timestamp.append(timestamp)
-# occurance.append(occurance)
-
-
-# df = pd.DataFrame({'Time':time,'Occurance':occurance})
-# df.to_csv('products.csv', index=False, encoding='utf-8')
