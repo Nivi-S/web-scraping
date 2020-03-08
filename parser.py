b171e08d9b4c20c05fcb9015a8c2124c230f18e0
Jeannie Huang <jhuang@cs.ucla.edu>
2020-03-08 00:41:15 -0800

    Extracting stats from sentences for more precise NLP

--- parser.py
+++ parser.py
@@ -1,82 +1,123 @@
-#from selenium import webdriver
+# from selenium import webdriver
 from bs4 import BeautifulSoup
 import re
 import requests
 import pressSource as ps
 import urllib3
-#import flask
+# import flask
 
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-def parseHTML(str_url):
 
-    content = requests.get(str_url, verify = False)
+def scrapeHTML(str_url):
+    # Check that the URL is valid
+    content = requests.get(str_url, verify=False)
+    if content.status_code != 200:
+        print("Error in HTTP Request. Exiting with status code ",
+              content.status_code)
+        exit(1)
+
     soup = BeautifulSoup(content.text, 'html.parser')
 
-
+    # Create a press source object based on the string url
     if bool(re.search('.*cdc.*', str_url)):
-        searched_word = 'Total'
+        press_source = ps.pressSource("CDC", str_url)
 
-    if bool(re.search('.*usc.*', str_url)):
-        searched_word = 'STATUS'
+    elif bool(re.search('.*usc.*', str_url)):
+        press_source = ps.pressSource("USC", str_url)
 
-    if bool(re.search('.*lacounty.*', str_url)):
-        searched_word = 'total'
+    elif bool(re.search('.*lacounty.*', str_url)):
+        press_source = ps.pressSource("LA", str_url)
 
-    if bool(re.search('.*ucla.*', str_url)):
-        searched_word = 'STATUS'
+    elif bool(re.search('.*ucla.*', str_url)):
+        press_source = ps.pressSource("UCLA", str_url)
 
-    if bool(re.search('.*cdph.*', str_url)):
-        searched_word = 'positive\scases'
+    elif bool(re.search('.*cdph.*', str_url)):
+        press_source = ps.pressSource("CA", str_url)
+    else:
+        print("Site not recognized. We are not monitoring this site yet. Exiting")
+        exit(1)
 
+    # Set the date value
+    date_sentences = soup.find_all(string=re.compile(
+        '[ADFJMNOS]\w* [\d]{1,2},* [\d]{4}'))
 
-    results = soup.find_all(string=re.compile(
-        '.*{0}.*'.format(searched_word)), recursive=True)
-    
-    results = [el.replace('\xa0',' ') for el in results]
-    #for x in results:
-       # x  = x.replace(u'\xa0', u' ')
-        #results.append(x)
+    if len(date_sentences) == 0:
+        date = ""
+    else:
+        date = re.search(
+            '[ADFJMNOS]\w* [\d]{1,2},* [\d]{4}', date_sentences[0]).group()
 
-    print('results =', results)
-
-    date = soup.find_all(string=re.compile('[ADFJMNOS]\w* [\d]{1,2},* [\d]{4}'))
-    print("Date=", date[0])
-
-    for content in results:
-        words = content.split()
-        for index, word in enumerate(words):
-            # If the content contains the search word twice or more this will fire for each occurence
-            if word == searched_word:
-                print("Content= ", content, "\n")#('Results: "{0}"'.format(content))
-
-    #print("\n Title =", soup.title.text)
+    press_source.date = date
+    print("Date = ", date)
+    print(press_source.date)
     print("Source =", str_url, "\n ================ \n")
 
-    if bool(re.search('.*cdc.*', str_url)):
-        res = ps.pressSource(ps.level.NATIONAL, str_url)
+    # Return sentences containing these hit words
+    hit_words = {'STATUS', 'status', 'Total', 'total', 'cases'}
+    hit_results = []
+    for word in hit_words:
+        hit_results.extend(soup.find_all(string=re.compile(
+            '.*{0}.*'.format(word)), recursive=True))
 
-    if bool(re.search('.*usc.*', str_url)):
-        res = ps.pressSource(ps.level.SCHOOL, str_url)
+    hit_results = [el.replace('\xa0', ' ') for el in hit_results]
 
-    if bool(re.search('.*lacounty.*', str_url)):
-        res = ps.pressSource(ps.level.COUNTY, str_url)
+    # print('hit_results =', hit_results)
+    print("Hit results:")
+    for i, hit in enumerate(hit_results):
+        print(i, hit)
 
-    if bool(re.search('.*ucla.*', str_url)):
-        res = ps.pressSource(ps.level.SCHOOL, str_url)
+    # Further parse sentences with hit words to extract number of positive and death cases
+    positive_words = {'positive cases', 'Positive cases',
+                      'confirmed cases', 'cases confirmed', 'Total cases',
+                      'total cases', 'total number of cases', 'Total number of cases'}
+    death_words = {'deaths', 'death'}
 
-    #res = ps.pressSource(ps.level.NATIONAL, str_url)
+    # Examine words one or two away from the hit
+    positive_phrase_locations = []
+    negative_phrase_locations = []
+    delta = 40
+    for idx_sent, sentence in enumerate(hit_results):
+        for word in positive_words:
+            idx_char = sentence.find(word)
+            if idx_char != -1:
+                search_range = (max(0, idx_char - delta),
+                                min(len(sentence)-1, idx_char + delta))
+                print(idx_sent, sentence[search_range[0]:search_range[1]])
+                print(
+                    re.match('.*\s[1-9]*.*', sentence[search_range[0]:search_range[1]]).group(0))
 
+                positive_phrase_locations.append((idx_sent, idx_char))
+        for word in death_words:
+            idx_char = sentence.find(word)
+            if idx_char != -1:
+                negative_phrase_locations.append((idx_sent, idx_char))
 
+    #print(positive_phrase_locations, negative_phrase_locations)
 
 
 if __name__ == "__main__":
     url = 'https://www.cdc.gov/coronavirus/2019-ncov/cases-in-us.html'
-    parseHTML(url)
-    parseHTML('https://sites.usc.edu/coronavirus/')
-    parseHTML('http://publichealth.lacounty.gov/media/Coronavirus/')
-    parseHTML('https://newsroom.ucla.edu/stories/coronavirus-information-for-the-ucla-campus-community')
-    parseHTML('https://www.cdph.ca.gov/Programs/CID/DCDC/Pages/Immunization/ncov2019.aspx')
+    scrapeHTML(url)
+    # scrapeHTML('https://sites.usc.edu/coronavirus/')
+    # scrapeHTML('http://publichealth.lacounty.gov/media/Coronavirus/')
+    # scrapeHTML(
+    #    'https://newsroom.ucla.edu/stories/coronavirus-information-for-the-ucla-campus-community')
+    # scrapeHTML(
+    #    'https://www.cdph.ca.gov/Programs/CID/DCDC/Pages/Immunization/ncov2019.aspx')
 
 # ============================
+
+    # print("*****************************", date)
+    # print("Date =", date[0])
+
+    # for res in hit_results:
+    #     words = res.split()
+    #     for index, word in enumerate(words):
+    #         # If the content contains the search word twice or more this will fire for each occurence
+    #         if word == searched_word:
+    #             # ('Results: "{0}"'.format(content))
+    #             print("Content= ", content, "\n")
+
+    # print("\n Title =", soup.title.text)
