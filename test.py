1d990648a7a029207050908d18ab88d94481985f
Jeannie Huang <jhuang@cs.ucla.edu>
2020-03-07 20:47:38 -0800

    remove test file

--- test.py
+++
@@ -1,8 +0,0 @@
-from selenium import webdriver
-from bs4 import BeautifulSoup
-import requests
-
-url = "https://en.wikipedia.org/wiki/List_of_countries_by_GDP_(nominal)"
-html_content = requests.get(url).text
-soup = BeautifulSoup(html_content, "lxml")
-print(soup.title.text)
